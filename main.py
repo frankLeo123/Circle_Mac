69c7dc50e959f28f6990e4ca21bfd1f6f2d2ebdb
frankdemo123 <majh@ios.ac.cn>
2019-07-09 01:23:50 +0800

    更新，可以通过滚动条动态调节

--- main.py
+++ main.py
@@ -2,30 +2,54 @@ import cv2
 import os
 import numpy as np
 
+
+def nothing(x):
+    pass
 # 读取
 img = cv2.imread("./config/test.jpg")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-edges = cv2.Canny(gray,200,30)
-
-circle = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,5,param1=200,param2=30
-                          ,minRadius=20,maxRadius=25)
-
-
-num_circle = 0
-if circle is not None:
-    circle = np.uint16(np.around(circle[0,:,:]))
-    for i in circle[:]:
-        # Draw outer circle
-        cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        # Draw inner circle
-        cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
-        num_circle = num_circle + 1
-    print(img.shape[0]//5)
-    # cv2.putText(img,str(num_circle),(200,200),3,3,(0,0,0))
-    cv2.putText(img,str(num_circle),(img.shape[1]//3,img.shape[0]//7),6,4,(0,0,0))
-    # cv2.putText(img,)
-# cv2.imshow("result",img)
+# ret,gray = cv2.threshold(gray_,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# cv2.imshow("demo",gray)
 # cv2.waitKey(0)
-cv2.imwrite("./debug/edge3.png",edges)
-cv2.imwrite("./debug/result1.png",img)
+
+gray = cv2.resize(gray,None,fx=0.5,fy=0.5,interpolation=cv2.INTER_LINEAR)
+img = cv2.resize(img,None,fx=0.5,fy=0.5,interpolation=cv2.INTER_LINEAR)
+cv2.namedWindow("circle")
+cv2.createTrackbar('param_canny1','circle',0,200,nothing)
+cv2.createTrackbar('param_canny2','circle',0,200,nothing)
+cv2.createTrackbar('param_Hough1','circle',15,200,nothing)
+cv2.createTrackbar('param_Hough2','circle',15,100,nothing)
+
+
+while (True):
+    gray_ = np.copy(gray)
+    img_ = np.copy(img)
+    param_canny1 = cv2.getTrackbarPos("param_canny1",'circle')
+    param_canny2 = cv2.getTrackbarPos("param_canny2",'circle')
+    param_Hough1 = cv2.getTrackbarPos("param_Hough1",'circle')
+    param_Hough2 = cv2.getTrackbarPos("param_Hough2",'circle')
+    # print(param_Hough2.type())
+    # a = param_Hough1
+    print(param_canny1,param_canny2)
+    edges = cv2.Canny(gray_,param_canny1,param_canny2)
+    circle = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,1,5,param1=param_Hough1,param2=param_Hough2,minRadius=20//2,maxRadius=25//2)
+    num_circle = 0
+    if circle is not None:
+        circle = np.uint16(np.around(circle[0,:,:]))
+        for i in circle[:]:
+            # Draw outer circle
+            cv2.circle(img_, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # Draw inner circle
+            cv2.circle(img_, (i[0], i[1]), 2, (0, 0, 255), 3)
+            num_circle = num_circle + 1
+        print(img.shape[0]//5)
+        cv2.putText(img_,str(num_circle),(img_.shape[1]//3,img_.shape[0]//7),6,4,(0,0,0))
+    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    dst = np.hstack((edges,img_))
+    # cv2.imshow("circle", edges)
+    cv2.imshow("circle", dst)
+    if cv2.waitKey(1) == 27:
+        cv2.imwrite("./debug/dst.png",dst)
+        break
+    # cv2.imwrite("./debug/result3.png",img)
