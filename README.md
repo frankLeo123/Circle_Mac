07614034e9691bc9394f60389add753fc5a9c5aa
junhui <majh@ios.ac.cn>
2019-07-09 10:53:09 +0800

    增加了调试，整理了代码

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Circle_Mac
 find an interesting question from zhihu, which about how to discover all of circle in mac 
-![image](config/test.jpg)
-![image](./debug/result1.png)
+![image](./debug/dst.png)
+![image](./BarProgress.png)
 
--- main.py
+++ main.py
@@ -9,12 +9,11 @@ def nothing(x):
 img = cv2.imread("./config/test.jpg")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-# ret,gray = cv2.threshold(gray_,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-# cv2.imshow("demo",gray)
-# cv2.waitKey(0)
-
-gray = cv2.resize(gray,None,fx=0.5,fy=0.5,interpolation=cv2.INTER_LINEAR)
-img = cv2.resize(img,None,fx=0.5,fy=0.5,interpolation=cv2.INTER_LINEAR)
+# OTUS 设置阈值后canny，更为精准
+ret,gray = cv2.threshold(gray,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# 方便调试，缩小一倍
+# gray = cv2.resize(gray,None,fx=0.5,fy=0.5,interpolation=cv2.INTER_LINEAR)
+# img = cv2.resize(img,None,fx=0.5,fy=0.5,interpolation=cv2.INTER_LINEAR)
 cv2.namedWindow("circle")
 cv2.createTrackbar('param_canny1','circle',0,200,nothing)
 cv2.createTrackbar('param_canny2','circle',0,200,nothing)
@@ -29,11 +28,12 @@ while (True):
     param_canny2 = cv2.getTrackbarPos("param_canny2",'circle')
     param_Hough1 = cv2.getTrackbarPos("param_Hough1",'circle')
     param_Hough2 = cv2.getTrackbarPos("param_Hough2",'circle')
-    # print(param_Hough2.type())
-    # a = param_Hough1
-    print(param_canny1,param_canny2)
+
+    print(param_canny1,param_canny2,param_Hough1,param_Hough2)
     edges = cv2.Canny(gray_,param_canny1,param_canny2)
-    circle = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,1,5,param1=param_Hough1,param2=param_Hough2,minRadius=20//2,maxRadius=25//2)
+    circle = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,1,5,param1=param_Hough1,param2=param_Hough2,minRadius=20,maxRadius=25)
+    # 缩小一倍
+    # circle = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,1,5,param1=param_Hough1,param2=param_Hough2,minRadius=20//2,maxRadius=25//2)
     num_circle = 0
     if circle is not None:
         circle = np.uint16(np.around(circle[0,:,:]))
@@ -45,8 +45,9 @@ while (True):
             num_circle = num_circle + 1
         print(img.shape[0]//5)
         cv2.putText(img_,str(num_circle),(img_.shape[1]//3,img_.shape[0]//7),6,4,(0,0,0))
-    img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
-    dst = np.hstack((edges,img_))
+    # img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    edges_ = cv2.merge((edges,edges,edges))
+    dst = np.hstack((edges_,img_))
     # cv2.imshow("circle", edges)
     cv2.imshow("circle", dst)
     if cv2.waitKey(1) == 27:
